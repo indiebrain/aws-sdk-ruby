79c07b7cea1ffe7497820b592663023673d10e85
Aaron Kuehler <aaron.kuehler@powerhrg.com>
2025-05-19 15:25:27 -0400

    Make sure we're loading the correct version of the gem

--- gems/aws-sdk-s3/lib/aws-sdk-s3/multipart_stream_uploader.rb
+++ gems/aws-sdk-s3/lib/aws-sdk-s3/multipart_stream_uploader.rb
@@ -45,6 +45,9 @@ module Aws
       # @option options [required,String] :key
       # @return [Seahorse::Client::Response] - the CompleteMultipartUploadResponse
       def upload(options = {}, &block)
+        puts "----------"
+        puts " <-------------- Here is the wumpus"
+        puts ----------
         Aws::Plugins::UserAgent.feature('s3-transfer') do
           upload_id = initiate_upload(options)
           parts = upload_parts(upload_id, options, &block)
