86834b3240525c9563a1e1e9af2343407a6db42b
Aaron Kuehler <aaron.kuehler@powerhrg.com>
2025-05-21 11:40:19 -0400

    Remove noisy, unhelpful logs

--- gems/aws-sdk-s3/lib/aws-sdk-s3/plugins/sse_cpk.rb
+++ gems/aws-sdk-s3/lib/aws-sdk-s3/plugins/sse_cpk.rb
@@ -20,7 +20,6 @@ This should only be disabled for local testing.
         class Handler < Seahorse::Client::Handler
 
           def call(context)
-            puts "Aws::S3::Plugins::SseCpk#call #{context}"
             compute_key_md5(context) if context.params.is_a?(Hash)
             @handler.call(context)
           end
@@ -28,16 +27,13 @@ This should only be disabled for local testing.
           private
 
           def compute_key_md5(context)
-            puts "Aws::S3::Plugins::SseCpk#compute_key_md5 #{context}"
             params = context.params
             if key = params[:sse_customer_key]
-              puts "Aws::S3::Plugins::SseCpk#compute_key_md5 sse_customer_key #{key}"
               require_https(context)
               params[:sse_customer_key] = base64(key)
               params[:sse_customer_key_md5] = base64(md5(key))
             end
             if key = params[:copy_source_sse_customer_key]
-              puts "Aws::S3::Plugins::SseCpk#compute_key_md5 copy_source_sse_customer_key #{key}"
               require_https(context)
               params[:copy_source_sse_customer_key] = base64(key)
               params[:copy_source_sse_customer_key_md5] = base64(md5(key))
@@ -45,7 +41,6 @@ This should only be disabled for local testing.
           end
 
           def require_https(context)
-            puts "Aws::S3::Plugins::SseCpk#require_https config endpoint #{context.config.endpoint}"
             unless URI::HTTPS === context.config.endpoint
               msg = <<-MSG.strip.gsub("\n", ' ')
                 Attempting to send customer-provided-keys for S3
